e816141c1b2f70acaefce781713c88690713fa88
Michael <michael.barry.email@gmail.com>
2021-03-24 00:00:50 -0600

    fixing routing bug

--- frontend/src/screens/AllProductsScreen.js
+++ frontend/src/screens/AllProductsScreen.js
@@ -96,6 +96,7 @@ const AllProductsScreen = ({ match }) => {
               ))}
             </Row>
             <Paginate
+              filter={filter}
               pages={pages}
               page={page}
               keyword={keyword ? keyword : ""}
